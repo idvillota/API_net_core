62a3bc3983ea5b2a4d423cfe55f102d2ecd16cf0
Ivan Dario Villota <idvillota@gmail.com>
2019-03-14 23:38:17 -0500

    Add model validations.

--- api/Controllers/PointsOfInterestController.cs
+++ api/Controllers/PointsOfInterestController.cs
@@ -52,6 +52,17 @@ namespace api.Controllers
                 return BadRequest();
             }
 
+            //Custom validations:
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "Th provider description should be different from the name.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
             if (city == null)
             {
